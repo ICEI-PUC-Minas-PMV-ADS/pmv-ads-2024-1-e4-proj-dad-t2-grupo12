5588ee05768c1f8e8fb304e9479584fe93811387
Leonardo Igor <115428616+leosportt@users.noreply.github.com>
2024-04-03 23:36:32 -0300

    Add files via upload
    API-Controller

---
+++ src/API-Controller/API-Controller/Models/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API_Controller.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Cargo { get; set; }
+        public DateTime DataContratacao { get; set; }
+    }
+}
---
+++ src/API-Controller/API-Controller/Models/RegistroPonto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace API_Controller.Models
+{
+    public class RegistroPonto
+    {
+        public int Id { get; set; }
+        public int FuncionarioId { get; set; }
+        public DateTime DataHora { get; set; }
+        public TipoRegistroPonto Tipo { get; set; }
+    }
+
+    public enum TipoRegistroPonto
+    {
+        Entrada,
+        Saida
+    }
+}
