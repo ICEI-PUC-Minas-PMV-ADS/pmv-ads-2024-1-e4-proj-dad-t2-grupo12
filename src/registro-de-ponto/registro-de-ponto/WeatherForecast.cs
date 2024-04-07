aa7716d29ec9968f44d331044302ae92625333d9
Renato Gualberto <gprenatofreitas@gmail.com>
2024-04-07 13:24:05 -0300

    Commit inicial

---
+++ src/registro-de-ponto/registro-de-ponto/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace registro_de_ponto
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
