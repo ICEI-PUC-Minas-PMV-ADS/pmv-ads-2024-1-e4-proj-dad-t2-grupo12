60f4e38fca58a423b13ee95f6ca6811b4ca18fa0
Renato Gualberto <gprenatofreitas@gmail.com>
2024-04-07 11:50:34 -0300

    Revert "inserido o registro de ponto_verificar o uso. A versão utilizada dos Entity é a versão 6.5 (mesa da aula do professor Kleber - MicroFundamento)"
    This reverts commit 225ff7396d63f5fededa51efddb6fc76573b7062.

---
+++ src/api-registro-de-ponto/api-registro-de-ponto/Controllers/FuncionarioController.cs
@@ -0,0 +1,63 @@
+﻿using api_RegistroDePonto.Models;
+using api_RegistroDePonto.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_RegistroDePonto.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FuncionarioController : ControllerBase
+    {
+        private readonly FuncionarioService _FuncionarioService;
+
+        public FuncionarioController(FuncionarioService FuncionarioService)
+        {
+            _FuncionarioService = FuncionarioService;
+        }
+
+        [HttpGet]
+        public async Task<List<Funcionario>> Get() =>
+            await _FuncionarioService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Funcionario>> Get(string id)
+        {
+            var Funcionario = await _FuncionarioService.GetAsync(id);
+            if (Funcionario == null)
+            {
+                return NotFound();
+            }
+
+            return Funcionario;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Funcionario newFuncionario)
+        {
+            await _FuncionarioService.CreateAsync(newFuncionario);
+
+            return CreatedAtAction(nameof(Get), new { id = newFuncionario.Id }, newFuncionario);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Funcionario updatedFuncionario)
+        {
+            var Funcionario = await _FuncionarioService.GetAsync(id);
+            if (Funcionario is null)
+                return NotFound();
+            updatedFuncionario.Id = Funcionario.Id;
+            await _FuncionarioService.UpdateAsync(id, updatedFuncionario);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var Funcionario = await _FuncionarioService.GetAsync(id);
+            if (Funcionario is null)
+                return NotFound();
+            await _FuncionarioService.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
