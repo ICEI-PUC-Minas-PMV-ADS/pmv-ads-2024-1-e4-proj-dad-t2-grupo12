ce12906c4d13261690e7d417fdb6664f4fea996d
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-02 21:23:54 -0300

    Adição de itens e componentes na tela de visualização do registro diario

--- src/ponto-web/src/pages/registro-diario-colaborador/RegistroDiarioColaborador.jsx
+++ src/ponto-web/src/pages/registro-diario-colaborador/RegistroDiarioColaborador.jsx
@@ -2,11 +2,12 @@ import './RegistroDiarioColaborador.css';
 import MenuLateral from "../../components/menu-lateral/MenuLateral.jsx";
 import Timeline from "../../components/timeline/Timeline.jsx";
 import Header from "../../components/header/Header.jsx";
+import StatusSelector from "../../components/status-selector/StatusSelector.jsx";
 
 const RegistroDiarioColaborador = () => {
     return (
         <div className="app">
-           <Header></Header>
+            <Header></Header>
             <div className="container">
                 <div className="menu-lateral">
                     <MenuLateral></MenuLateral>
@@ -35,18 +36,43 @@ const RegistroDiarioColaborador = () => {
                                 <Timeline></Timeline>
                             </div>
                             <div className="vertical-line-painel"></div>
-                            <div className="motivo-div">
-                                <span className="motivo-titulo">Motivo da solitação:</span>
-                                <div className="card-motivo">
-                                    <span className="motivo-texto">Esqueci de registrar a volta do almoço</span>
+                            <div className="div-central">
+                                <div className="motivo-div">
+                                    <span className="motivo-titulo">Motivo da solicitação:</span>
+                                    <div className="card-motivo">
+                                        <span className="motivo-texto">Esqueci de registrar a volta do almoço</span>
+                                    </div>
                                 </div>
+                                <div className="saldo-informacao">
+                                    <div className="saldo-item">
+                                        <span className="saldo-label">Saldo diário:</span>
+                                        <span className="saldo-valor">+02h 20m</span>
+                                    </div>
+                                    <div className="saldo-item">
+                                        <span className="saldo-label">Saldo diário:</span>
+                                        <span className="saldo-valor">+02h 20m</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="vertical-line-painel"></div>
+                            <div className="botoes-div">
+                                <div className="botoes-titulo">
+                                    <span className="motivo-titulo">Alterar Status:</span>
+                                </div>
+                                <div className="botoes-select-status botoes-container">
+                                    <StatusSelector></StatusSelector>
+                                </div>
+                            </div>
+                            <div className="save-button">
+                                <button type="button" className="btn btn-warning btn-save">Salvar</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
+        ;
 }
 
 export default RegistroDiarioColaborador;
