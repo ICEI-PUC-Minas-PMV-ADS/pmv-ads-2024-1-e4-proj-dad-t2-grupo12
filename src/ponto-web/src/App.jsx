06961e1c9307ae5cb292f108edd7fc88c9fa8a98
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-04 18:31:14 -0300

    Implementação inicial de navegação entre as páginas

--- src/ponto-web/src/App.jsx
+++ src/ponto-web/src/App.jsx
@@ -1,14 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import PaginaInicial from "./pages/pagina-inicial/PaginaInicial.jsx";
+import { Routes, Route } from "react-router-dom";
+import PainelPontosColaborador from "./pages/painel-pontos-colaborador/PainelPontosColaborador.jsx";
 
 function App() {
-
   return (
     <>
-      <div>
-          <PaginaInicial></PaginaInicial>
-      </div>
+    <Routes>
+        <Route path='/' element={<PaginaInicial />} />
+        <Route path='painel-colaborador' element={<PainelPontosColaborador />} />
+    </Routes>
     </>
   )
 }
--- src/ponto-web/src/components/menu-lateral/MenuLateral.jsx
+++ src/ponto-web/src/components/menu-lateral/MenuLateral.jsx
@@ -1,11 +1,14 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import './MenuLateral.css';
+import { useNavigate} from "react-router-dom";
 
 function MenuLateral() {
+    const navigateTo = useNavigate()
+
     return (
         <Dropdown.Menu show>
-            <Dropdown.Item eventKey="2">Página inicial</Dropdown.Item>
-            <Dropdown.Item eventKey="3">Solicitações</Dropdown.Item>
+            <Dropdown.Item eventKey="2" onClick={() => navigateTo(`/`)}>Página inicial</Dropdown.Item>
+            <Dropdown.Item eventKey="3" onClick={() => navigateTo(`painel-colaborador`)}>Solicitações </Dropdown.Item>
             <Dropdown.Item eventKey="4">Cadastrar/Editar colaborador</Dropdown.Item>
             <Dropdown.Item eventKey="4">Perfil</Dropdown.Item>
         </Dropdown.Menu>
