4ba80841e4a180ec77bdbecc4e6e924841ea0ae7
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-04 19:50:53 -0300

    Correção navegação menu lateral

--- src/ponto-web/src/components/menu-lateral/MenuLateral.jsx
+++ src/ponto-web/src/components/menu-lateral/MenuLateral.jsx
@@ -1,14 +1,23 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import './MenuLateral.css';
-import { useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 function MenuLateral() {
     const navigateTo = useNavigate()
+    const location = useLocation();
+
+    const handleNavigateTo = (path) => {
+        const caminho = '/' + path;
+
+        if (location.pathname !== caminho) {
+            navigateTo(path);
+        }
+    };
 
     return (
         <Dropdown.Menu show>
-            <Dropdown.Item eventKey="2" onClick={() => navigateTo(`/`)}>Página inicial</Dropdown.Item>
-            <Dropdown.Item eventKey="3" onClick={() => navigateTo(`painel-colaborador`)}>Solicitações </Dropdown.Item>
+            <Dropdown.Item eventKey="2" onClick={() => handleNavigateTo(`/`)}>Página inicial</Dropdown.Item>
+            <Dropdown.Item eventKey="3" onClick={() => handleNavigateTo(`painel-colaborador`)}>Solicitações </Dropdown.Item>
             <Dropdown.Item eventKey="4">Cadastrar/Editar colaborador</Dropdown.Item>
             <Dropdown.Item eventKey="4">Perfil</Dropdown.Item>
         </Dropdown.Menu>
