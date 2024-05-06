07e0e0e9d085ff66e35370fc98a139a8ae57a75d
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-05 22:15:25 -0300

    Ajustes perfil e login

--- src/ponto-web/src/components/painel-perfil/Perfil.jsx
+++ src/ponto-web/src/components/painel-perfil/Perfil.jsx
@@ -6,31 +6,30 @@ const Perfil = () => {
     return (
         <div className="perfil-container">
             <div className="perfil-dados-fixo">
-                <img src="" alt="Foto do usuário" className="perfil-foto"/>
+                <img src="src/assets/exemplo.jpeg" alt="Foto do usuário" className="perfil-foto"/>
                 <input type="text" value="Usuário Teste" disabled className="input-disabled"/>
                 <input type="text" value="usuario@teste.com" disabled className="input-disabled"/>
                 <input type="text" value="000.000.000-00" disabled className="input-disabled"/>
                 <button className="btn-resetar">Resetar Senha</button>
             </div>
             <div className="perfil-dados-editaveis">
-            <label>
+            <label className="label-perfil">
                     Setor:
-                    <select defaultValue="TI" className="input-editavel">
+                    <select defaultValue="Comercial" className="input-editavel">
                         <option value="Comercial">Comercial</option>
                         <option value="Operacional">Operacional</option>
                         <option value="RH">RH</option>
                         <option value="Logistica">Logística</option>
-                        <option value="TI">TI</option> {/* Incluído para manter a entrada padrão */}
                     </select>
                 </label>
-                <label>
+                <label className="label-perfil">
                     Status:
                     <select defaultValue="Ativo" className="input-editavel">
                         <option value="Ativo">Ativo</option>
                         <option value="Inativo">Inativo</option>
                     </select>
                 </label>
-                <label>Salário: <input type="text" defaultValue="R$ 5000,00" className="input-editavel2"/></label>
+                <label  className="label-perfil">Salário: <input type="text" defaultValue="R$ 5000,00" className="input-editavel2"/></label>
             </div>
         </div>
     );
