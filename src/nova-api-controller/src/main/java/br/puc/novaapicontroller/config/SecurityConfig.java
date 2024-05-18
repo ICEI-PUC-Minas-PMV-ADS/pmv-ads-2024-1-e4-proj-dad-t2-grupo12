9f951c1f8c980dc15eb2b0af8b87058c8c3dde62
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-18 18:54:33 -0300

    Revert "Teste"
    This reverts commit 131633f54494fbbc2c820801a724fcc2edf0ab9c.

--- src/nova-api-controller/src/main/java/br/puc/novaapicontroller/config/SecurityConfig.java
+++ src/nova-api-controller/src/main/java/br/puc/novaapicontroller/config/SecurityConfig.java
@@ -9,9 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,38 +16,20 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN", "USER")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests(auth -> {
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+//                    auth.requestMatchers("/v1/public/**").permitAll();
                     auth.requestMatchers("/**").permitAll();
-                    auth.requestMatchers("/user").hasRole("USER");
-                    auth.requestMatchers("/admin").hasRole("ADMIN");
                 })
-                .httpBasic(withDefaults())
                 .build();
     }
 
