f590a159dbf1ebd773f541ca5f7936fcd1c24a99
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-18 21:40:20 -0300

    Update SecurityConfig.java

--- src/nova-api-controller/src/main/java/br/puc/novaapicontroller/config/SecurityConfig.java
+++ src/nova-api-controller/src/main/java/br/puc/novaapicontroller/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package br.puc.novaapicontroller.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +28,8 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
