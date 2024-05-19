9c360d263d1d16892ecf073eaa3d66d79d555526
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2024-05-18 21:29:17 -0300

    Update SecurityConfig.java

--- src/nova-api-controller/src/main/java/br/puc/novaapicontroller/config/SecurityConfig.java
+++ src/nova-api-controller/src/main/java/br/puc/novaapicontroller/config/SecurityConfig.java
@@ -19,13 +19,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
