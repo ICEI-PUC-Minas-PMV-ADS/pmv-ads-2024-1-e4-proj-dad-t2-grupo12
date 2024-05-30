83c0b5c9d6f9360208b7add18debcc3b24efd8e3
Jhony <scostajhony@gmail.com>
2024-05-30 14:34:28 -0300

    tela de perfil

--- src/ponto-mobile/App.js
+++ src/ponto-mobile/App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PaginaInicial from "./src/pages/PaginaInicial";
 import TabelaPonto from "./src/pages/TabelaPonto";
+import Profile from './src/pages/Profile';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TabelaPonto></TabelaPonto>
-      <StatusBar style="auto" />
+      <Profile></Profile>
     </View>
   );
 }
